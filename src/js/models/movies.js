8791762290499c5e4e949d7def19a5919af4d483
unknown <mysocialmedia69@yahoo.com>
2020-08-07 03:07:45 +0100

    update

--- src/js/models/movies.js
+++ src/js/models/movies.js
@@ -12,15 +12,7 @@ class Movies {
 
         const url = `https://api.themoviedb.org/3/movie/popular?api_key=ca357c71903c409f2ce08d61e75700a6&language=en-US&page=1`
 
-
-        const data = await axios.get(url).then(response => response.data);
-
-        return data;
-        // get movies from api
-
-        // const data = await fetch(url).then(response => response.json()).then(result => result.results);
-
-        return data;
+        console.log(url);
     }
 }
 
